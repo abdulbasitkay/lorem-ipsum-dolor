9fcab977ffb0cef03e3759ec68f8e0003eb6282d
andela-vdugeri <daniel.verem@icorpers.org>
2016-09-25 23:19:15 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var models = require('./server/models');
+var exit = require('gulp-exit');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+
+
+var paths = {
+  serverTest: './test/server/**/*.js',
+  serverFiles: './server/**/*.js'
+};
+
+gulp.task('coverage-setup', function () {
+  return gulp.src(paths.serverFiles)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('db:sync', function () {
+  return models.sequelize.sync().then(exit());
+});
+
+gulp.task('server:test',['db:sync', 'coverage-setup'], function () {
+  process.env.NODE_ENV = 'test';
+  return gulp.src(paths.serverTest)
+    .pipe(mocha())
+    .pipe(istanbul.writeReports({
+      dir: './test/coverage'
+    }));
+});
+
+gulp.task('test', ['server:test']);
