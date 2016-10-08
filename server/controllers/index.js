f3429c28072ff3d759e26ba2a2817079c2714733
andela-vdugeri <daniel.verem@icorpers.org>
2016-10-08 10:34:55 +0100

    Implement payment controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,10 +4,12 @@ var userController = require('./user.server.controller');
 var rolesController = require('./role.server.controller');
 var termController = require('./term.server.controller');
 var studentController = require('./student.server.controller');
+var paymentsController = require('./payment.server.controller');
 
 module.exports = {
   userController: userController,
   rolesController: rolesController,
   termController: termController,
-  studentController: studentController
+  studentController: studentController,
+  paymentsController: paymentsController
 };
---
+++ server/controllers/payment.server.controller.js
@@ -0,0 +1,68 @@
+var models = require('./../models');
+
+module.exports = {
+  index: index,
+  save: save,
+  find: find,
+  update: update,
+  destroy: destroy
+}
+
+
+function index(req, res) {
+  models.Payment.findAll().then(function (payments) {
+    return res.status(200).json(payments);
+  })
+  .catch(function (error) {
+    return res.status(500).json({ message: error.message });
+  });
+}
+
+function save(req, res) {
+  var payment = req.body.payment;
+  models.Payment.create(payment).then(function (payment) {
+    return res.status(200).json(payment);
+  })
+  .catch(function (error) {
+    return res.status(500).json({ message: error.message });
+  });
+}
+
+function find(req, res) {
+  var id = req.params.id;
+  models.Payment.findById(id).then(function (payment) {
+    return res.status(200).json(payment);
+  })
+  .catch(function (error) {
+    return res.status(500).json({ message: error.message });
+  });
+}
+
+function update(req, res) {
+  var id = req.params.id;
+  payment = req.body.payment;
+  models.Payment.update(payment, {
+    where: {
+      id: id
+    }
+  }).then(function (affectedRows) {
+    return res.status(200).json({ updated: true, payment: payment });
+  })
+  .catch(function (error) {
+    return res.status(500).json({ message: error.message });
+  });
+}
+
+function destroy(req, res) {
+  var id = req.params.id;
+  models.Payment.destroy({
+    where: {
+      id:id
+    }
+  }).then(function (affectedRows) {
+    return res.status(200).json({ message: 'Payment deleted' });
+  })
+  .catch(function (error) {
+    return res.status(500).json({ message: error.message });
+  });
+}
